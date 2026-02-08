01b1cbf2c5cc07838203a838b14768636dcac7cc
scrotor <dobrecht@DAVIDOBRECH6AF0>
2026-02-08 13:28:23 -0500

    Creating SDL window and game loop.

--- SDLPlatformer/SDLPlatformer.cpp
+++ SDLPlatformer/SDLPlatformer.cpp
@@ -1,19 +1,76 @@
 ﻿// SDLPlatformer.cpp : Defines the entry point for the application.
 //
 #include<SDL3/SDL.h>
+#include<SDL3/SDL_main.h>
 
 #include "SDLPlatformer.h"
 
 using namespace std;
 
-int main()
+struct SDL_State
 {
-	// Initialize the SDL video subsystem
-	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window *win;
+	SDL_Renderer *renderer;
+};
 
+void cleanup(SDL_Window* win);
+
+int main(int argc, char *argv[])
+{
+	int width = 800;
+	int height = 600;
+	bool isRunning = true;
+	SDL_State state;
+
+	// Initialize the SDL video subsystem
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Unable to initialize SDL", NULL);
+
+		return -1;
+	}
+
+	state.win = SDL_CreateWindow("SDL3 Demo", width, height, 0);
+
+	if (!state.win)
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Unable to create SDL Window", state.win);
+		cleanup(state.win);
+
+		return -1;
+	}
+
+	// Start the main game loop
+	while (isRunning)
+	{
+		SDL_Event event{ 0 };
+
+		while(SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+				case SDL_EVENT_QUIT:
+				{
+					isRunning = false;
+					break;
+				}
+				default:
+				{
+					break;
+				}
+			}
+		}
+	}
 	cout << "Hello CMake." << endl;
 
-	SDL_Quit();
+	cleanup(state.win);
 
 	return 0;
+}
+
+void cleanup(SDL_Window* win)
+{
+	SDL_DestroyWindow(win);
+
+	SDL_Quit();
 }
