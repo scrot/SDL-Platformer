79b8cc81c64e9212f6b9ee7e08ec56a3463402b1
scrotor <dobrecht@DAVIDOBRECH6AF0>
2026-02-08 12:10:46 -0500

    Add project files.

---
+++ SDLPlatformer/SDLPlatformer.cpp
@@ -0,0 +1,12 @@
+﻿// SDLPlatformer.cpp : Defines the entry point for the application.
+//
+
+#include "SDLPlatformer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ SDLPlatformer/SDLPlatformer.h
@@ -0,0 +1,8 @@
+﻿// SDLPlatformer.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
