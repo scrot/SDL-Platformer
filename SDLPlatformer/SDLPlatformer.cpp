bf194edaa960702dee14b8c60be7ed6955156796
scrotor <dobrecht@DAVIDOBRECH6AF0>
2026-02-08 12:33:11 -0500

    Configuring project to use SDL3.

--- SDLPlatformer/SDLPlatformer.cpp
+++ SDLPlatformer/SDLPlatformer.cpp
@@ -1,5 +1,6 @@
 ﻿// SDLPlatformer.cpp : Defines the entry point for the application.
 //
+#include<SDL3/SDL.h>
 
 #include "SDLPlatformer.h"
 
@@ -7,6 +8,12 @@ using namespace std;
 
 int main()
 {
+	// Initialize the SDL video subsystem
+	SDL_Init(SDL_INIT_VIDEO);
+
 	cout << "Hello CMake." << endl;
+
+	SDL_Quit();
+
 	return 0;
 }
