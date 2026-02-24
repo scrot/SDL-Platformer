bf184da4450f7d1674b3483fe8414a5493abf1f6
scrotor <dobrecht@DAVIDOBRECH6AF0>
2026-02-24 10:17:29 -0500

    Still adding Tiled support.

--- SDLPlatformer/tmx.cpp
+++ SDLPlatformer/tmx.cpp
@@ -32,12 +32,95 @@ std::unique_ptr<Map> tmx::loadMap(const std::string& mapFilePath)
 	{
 		if (strcmp(child->Name(), "tileset") == 0)
 		{
-			int firstGid = child->IntAttribute("firstgid");
+			int firstgid = child->IntAttribute("firstgid");
 
 			// Load tileset XML
 			XMLDocument tilesetDoc;
 			auto sourcePath = mapPath.parent_path().append(child->Attribute("source"));
 			tilesetDoc.LoadFile(sourcePath.string().c_str());
+
+			XMLElement* ts = tilesetDoc.FirstChildElement("tileset");
+
+			if (ts)
+			{
+				int tileWidth = ts->IntAttribute("tilewidth");
+				int tileHeight = ts->IntAttribute("tileheight");
+				int count = ts->IntAttribute("tilecount");
+				int columns = ts->IntAttribute("tilecolumns");
+
+				tmx::TileSet newTileset(firstgid, count, tileWidth, tileHeight, columns);
+
+				for (XMLElement* tile = tile->FirstChildElement("image"); tile != nullptr; tile = tile->NextSiblingElement("image"))
+				{
+					Tile newTile;
+					newTile.id = tile->IntAttribute("id");
+
+					XMLElement* image = tile->FirstChildElement("image");
+
+					if (image)
+					{
+						newTile.image.source = image->IntAttribute("image");
+						newTile.image.width = image->IntAttribute("width");
+						newTile.image.height = image->IntAttribute("height");
+					}
+
+					newTileset.tiles.push_back(newTile);
+				}
+
+			}
 		}
+		else
+			if (strcmp(child->Name(), "layer") == 0)
+			{
+				tmx::Layer layer;
+				layer.name = child->Attribute("name");
+				layer.id = child->IntAttribute("id");
+				layer.data.reserve(map->mapWidth * map->mapHeight);
+
+				XMLElement* data = child->FirstChildElement("data");
+				std::stringstream dataStream(data->GetText());
+
+				for (int i; dataStream >> i;)
+				{
+					layer.data.push_back(i);
+					
+					if (dataStream.peek() == ',')
+						dataStream.ignore();
+				}
+
+				map->layers.push_back(std::move(layer));
+			}
+			else
+				if (strcmp(child->Name(), "objectgroup") == 0)
+				{
+					tmx::ObjectGroup layer;
+					layer.name = child->Attribute("name");
+					layer.id = child->IntAttribute("id");
+
+					for (XMLElement* elem = child->FirstChildElement("object"); elem != nullptr; elem = elem->NextSiblingElement())
+					{
+						tmx::LayerObject obj;
+
+						obj.id = elem->IntAttribute("id");
+						obj.x = elem->FloatAttribute("x");
+						obj.y = elem->FloatAttribute("y");
+
+						const char* attrName = elem->Attribute("name");
+
+						if (attrName)
+							obj.name = attrName;
+
+						const char* attrType = elem->Attribute("type");
+
+						if (attrType)
+							obj.type = attrType;
+
+						layer.objects.push_back(obj);
+					}
+
+					map->layers.push_back(std::move(layer));
+				}
 	}
+
+	return std::unique_ptr<tmx::Map>(map);
 }
